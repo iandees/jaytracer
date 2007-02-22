5e9b45196aedfce54048170466dfef4991a349d1
ian.dees <ian.dees@b0fa1e7f-0a2a-0410-81b5-a5ab8a42a044>
2007-02-22 06:16:51 +0000

    Adding view plane.

--- trunk/JayTracer/com/yellowbkpk/jaytracer/world/Camera.java
+++ trunk/JayTracer/com/yellowbkpk/jaytracer/world/Camera.java
@@ -1,6 +1,7 @@
 package com.yellowbkpk.jaytracer.world;
 import javax.vecmath.Vector3f;
 
+import com.yellowbkpk.jaytracer.world.shape.Plane;
 import com.yellowbkpk.jaytracer.world.shape.Primitive;
 import com.yellowbkpk.jaytracer.world.shape.PrimitiveTypeEnum;
 
@@ -8,6 +9,8 @@ import com.yellowbkpk.jaytracer.world.shape.PrimitiveTypeEnum;
 public class Camera extends Primitive {
 
     private Vector3f lookAt;
+    private Plane viewPane;
+    
 
     private Camera(Vector3f loc) {
         super(loc, 0.0f);
@@ -16,6 +19,7 @@ public class Camera extends Primitive {
     public Camera(Vector3f camLoc, Vector3f camLookAt) {
         this(camLoc);
         lookAt = camLookAt;
+        viewPane = new Plane(camLookAt, 1.0f);
     }
 
     public PrimitiveTypeEnum getType() {
@@ -24,6 +28,10 @@ public class Camera extends Primitive {
 
     public Vector3f getLookAt() {
         return lookAt;
+    }
+
+    public Plane getViewPlane() {
+        return viewPane;
     }
 
 }
--- trunk/JayTracer/com/yellowbkpk/jaytracer/world/World.java
+++ trunk/JayTracer/com/yellowbkpk/jaytracer/world/World.java
@@ -3,6 +3,7 @@ import java.util.ArrayList;
 import java.util.Collection;
 
 import com.yellowbkpk.jaytracer.world.light.Light;
+import com.yellowbkpk.jaytracer.world.shape.Plane;
 import com.yellowbkpk.jaytracer.world.shape.Primitive;
 import com.yellowbkpk.jaytracer.world.shape.PrimitiveTypeEnum;
 import com.yellowbkpk.jaytracer.world.shape.Shape;
@@ -12,6 +13,7 @@ public class World {
 
     private Collection<Primitive> objects = new ArrayList<Primitive>();
     private Camera worldCamera = null;
+    private Plane worldViewPlane = null;
     private Collection<Light> worldLights = new ArrayList<Light>();
     private Collection<Shape> worldShapes = new ArrayList<Shape>();
 
@@ -19,10 +21,13 @@ public class World {
         
         if(PrimitiveTypeEnum.CAMERA.equals(shape.getType())) {
             worldCamera = (Camera) shape;
+            worldViewPlane = worldCamera.getViewPlane();
         } else if(PrimitiveTypeEnum.LIGHT.equals(shape.getType())) {
             worldLights.add((Light) shape);
         } else if(PrimitiveTypeEnum.SHAPE.equals(shape.getType())) {
             worldShapes.add((Shape) shape);
+        } else if(PrimitiveTypeEnum.VIEW_PLANE.equals(shape.getType())) {
+            worldViewPlane = (Plane) shape;
         }
         
         objects.add(shape);
@@ -38,6 +43,10 @@ public class World {
     
     public Collection<Shape> getShapes() {
         return worldShapes;
+    }
+
+    public Plane getViewPlane() {
+        return worldViewPlane;
     }
 
 }
